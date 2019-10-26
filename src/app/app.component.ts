1b014735e345bf9ac31b98709e190e76ac998be3
Kiyotaka <t10kiyotaka@gmail.com>
2019-10-26 09:58:23 +0900

    Impl suggestUserName method for patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,20 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   answer: '',
+    //   gender: 'male',
+    // });
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
