a42a71066030b1e87b0062f66b5eb5d4f69e5e5c
Kiyotaka <t10kiyotaka@gmail.com>
2019-10-27 12:34:32 +0900

    Modify submit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,5 +45,11 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.answer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
   }
 }
